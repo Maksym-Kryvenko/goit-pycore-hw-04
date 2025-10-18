45434c0ab397de16e1e7eae325c11aa3eb34f165
Maksym Kryvenko <maks1997ym@gmail.com>
2025-10-18 12:23:15 +0200

    Completed 3-4 tasks

---
+++ task3.py
@@ -0,0 +1,25 @@
+from colorama import Fore, Style
+import sys
+import pathlib
+
+def print_dir_structure(folder: str, indent: int = 0) -> None:
+    """
+    Print the directory structure of a given folder.
+    :param folder: Path to the folder.
+    :param indent: Current indentation level for printing.
+    """
+    folder_path = pathlib.Path(folder)
+    if folder_path.exists() and folder_path.is_dir():
+        print(' ' * (indent) + Fore.BLUE + folder_path.name + "/" + Style.RESET_ALL)
+        for item in folder_path.iterdir():
+            if item.is_dir() and not item.name.startswith('.'):
+                print_dir_structure(item, indent + 4)
+            else:
+                print(' ' * (indent + 4) + Fore.GREEN + item.name + Style.RESET_ALL)
+    else:
+        print(Fore.RED + "The specified folder does not exist." + Style.RESET_ALL)
+    
+
+
+
+print_dir_structure(sys.argv[1]) # "/Users/maksymkryvenko/Documents/education/neoversity/goit-pycore-hw-04/")
---
+++ task4.py
@@ -0,0 +1,93 @@
+class ContactBook:
+    """
+    A simple contact book to save, get, update, and show contacts.
+    """
+    def __init__(self):
+        self.contacts = {}
+
+    def save_contact(self, name: str, phone: str):
+        """
+        Save name and phone number.
+        """
+        self.contacts[name] = phone
+        print("Contact saved.")
+
+    def get_contact(self, name: str):
+        """
+        Get phone number by name.
+        """
+        print(self.contacts.get(name, "Contact not found."))
+
+    def update_contact(self, name: str, phone: str):
+        """
+        Update phone number for existing contact.
+        """
+        if name in self.contacts:
+            self.contacts[name] = phone
+            print("Contact updated.")
+        else:
+            print("Contact not found.")
+
+    def show_all_contacts(self):
+        """
+        Show all saved contacts.
+        """
+        for name, phone in self.contacts.items():
+            print(f"Name: {name}, Phone: {phone}")
+
+def parse_input(string: str):
+    """
+    Parse user input and return command and arguments.
+    """
+    parts = string.split(" ")
+    if len(parts) == 1:
+        return parts[0].lower(), []
+    elif len(parts[0]) > 1:
+        return parts[0].lower(), parts[1:]
+    else:  
+        return None, []
+
+def main():
+    """
+    Main function to run the contact book bot.
+    """
+    contact_book = ContactBook()
+    print("Welcome to the Contact Book Bot!")
+    print("""Available functions and commands: 
+          * save <name> <phone> - Save a new contact
+          * get <name> - Get a contact's phone number
+          * change/update <name> <phone> - Update an existing contact
+          * show all - Show all contacts
+          * close/exit - Exit the bot
+          """)
+    while True:
+        user_input = input("Enter command: ")
+        command, *args = parse_input(user_input)
+
+        if command in ["add", "save"]:
+            if len(args[0]) == 2:
+                contact_book.save_contact(*args[0])
+            else:
+                print("Please provide a name and phone number to save the contact.")
+        elif command in ["change", "update"]:
+            if len(args[0]) == 2:
+                contact_book.update_contact(*args[0])
+            else:
+                print("Please provide a name and phone number to update the contact.")
+        elif command in ["show", "phone"]:
+            if args[0]:
+                contact_book.get_contact(*args[0])
+            else:
+                print("Please provide a name to get the contact.")
+        elif command in ["show all", "all"]:
+            contact_book.show_all_contacts()
+        elif command in ["exit", "close", "good bye"]:
+            print("Good bye!")
+            break
+        elif command == "hello":
+            print("How can I help you?")
+        else:
+            print("Unknown command. Please try again.")
+
+if __name__ == "__main__":
+    main()
